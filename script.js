26cc598d6ab4ddc27d66c8c23a18b852d10b0b0d
John Bezustally <ample-flood.0p@icloud.com>
2022-02-28 12:34:58 +0400

    version 0.7.0

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const AMOUNT_OF_COLUMNS = 7
+const AMOUNT_OF_DAYS = 7
+const TODAY = new Date().toISOString().split('T')[0]
+
 // for prod
 // const taskUrl = 'https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/tasks'
 // const usersUrl = 'https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/users'
@@ -30,7 +32,7 @@ function defineDates(tasks) {
   }
   const firstColumn = earliestDate(tasks)
 
-  for (let i = 0; i < AMOUNT_OF_COLUMNS; i++) {
+  for (let i = 0; i < AMOUNT_OF_DAYS; i++) {
     let date = new Date(Date.parse(firstColumn) + (i * 86400000))
     date = date.toISOString().split('T')[0]
     dates.push(date)
@@ -40,34 +42,35 @@ function defineDates(tasks) {
   return dates
 }
 
+function separateTasks(tasks) {
+  const backlog = []
+  const assignedTasks = []
+  for (let i = 0; i < tasks.length; i++) {
+    if (!tasks[i].executor) {
+      backlog.push(tasks[i])
+    } else {
+      assignedTasks.push(tasks[i])
+    }
+  }
+  return { assignedTasks, backlog }
+}
+
 async function createApp() {
 
   const root = document.getElementById("root")
   const table = document.createElement('div')
   table.classList = "table"
-  table.style.gridTemplateColumns = `repeat(${AMOUNT_OF_COLUMNS + 1}, 1fr)`
+  table.style.gridTemplateColumns = `repeat(${AMOUNT_OF_DAYS + 1}, 1fr)`
 
   const tasks = await getData(taskUrl)
   const users = await getData(usersUrl)
-  const dates = defineDates(tasks)
+  let dates = defineDates(tasks)
 
-  function separateTasks(tasks) {
-    const backlog = []
-    const assignedTasks = []
-    for (let i = 0; i < tasks.length; i++) {
-      if (!tasks[i].executor) {
-        backlog.push(tasks[i])
-      } else {
-        assignedTasks.push(tasks[i])
-      }
-    }
-    return {assignedTasks, backlog}
-  }
   const { assignedTasks, backlog } = separateTasks(tasks)
 
   function createTable() {
     root.appendChild(table)
-    for (let i = 0; i <= AMOUNT_OF_COLUMNS; i++) {
+    for (let i = 0; i <= AMOUNT_OF_DAYS; i++) {
       const header = document.createElement('div')
       table.appendChild(header)
       for (let j = 0; j <= users.length; j++) {
@@ -80,9 +83,8 @@ async function createApp() {
 
   function createTableHeaders() {
     const headers = document.querySelectorAll('.table div :first-child')
-    const today = new Date().toISOString().split('T')[0]
     headers.forEach((header, idx) => {
-      if (dates[idx] === today) {
+      if (dates[idx] === TODAY) {
         header.parentElement.classList = 'today'
       }
       header.textContent = dates[idx]
@@ -98,6 +100,18 @@ async function createApp() {
     }
   }
 
+  function createPrevButton() {
+    prevButton = document.querySelector('.table :first-child :first-child')
+    prevButton.textContent = "Previous"
+    prevButton.classList.add("prev-btn")
+  }
+
+  function createNextButton() {
+    nextButton = document.querySelector('.table :last-child .table__header')
+    nextButton.textContent = "Next"
+    nextButton.classList.add("next-btn")
+  }
+
   function createCells() {
     const cells = document.querySelectorAll('.table__cell')
     for (let i = 0; i < cells.length; i++) {
@@ -110,26 +124,40 @@ async function createApp() {
   function assignTasks() {
     const cells = document.querySelectorAll('.table__cell')
     cells.forEach(cell => {
+      cell.textContent = ''
+      cell.classList = 'table__cell'
       assignedTasks.forEach(task => {
         if (task.planStartDate == cell.id.split(' ')[0] && task.executor == cell.id.split(' ')[1]) {
           cell.textContent = task.subject
-          cell.classList.add("table_cell-task")
+          cell.classList.toggle("table_cell-task")
           cell.id = task.id
         }
       })
     })
   }
 
-  function makePrevButton() {
-    prevButton = document.querySelector('.table :first-child :first-child')
-    prevButton.textContent = "Previous"
-    prevButton.classList.add("prev-btn")
-  }
+  function createTooltips() {
+    const cellsWithTask = document.querySelectorAll('.table_cell-task')
+    
+    cellsWithTask.forEach(cell => {
+      const tooltip = document.createElement('span')
+      tooltip.className = 'tooltip'
 
-  function makeNextButton() {
-    nextButton = document.querySelector('.table :last-child :first-child')
-    nextButton.textContent = "Next"
-    nextButton.classList.add("next-btn")
+      const taskId = cell.id
+
+      for (let j = 0; j < assignedTasks.length; j++) {
+        if (taskId === assignedTasks[j].id) {
+          currentTask = assignedTasks[j]
+          tooltip.innerHTML = `
+                              ${currentTask.description}
+                              <br><br>
+                              срок ${currentTask.planEndDate}
+                              `
+        }
+      }
+      cell.appendChild(tooltip)
+    })
+
   }
 
   function createBacklog() {
@@ -146,37 +174,60 @@ async function createApp() {
     }
   }
 
-  function createTooltips() {
-    const cellsWithTask = document.querySelectorAll('.table_cell-task')
-    for (let i = 0; i < cellsWithTask.length; i++) {
-      const tooltip = document.createElement('span')
-      tooltip.className = 'tooltip'
+  function createNavigation() {
+    prevBtn = document.getElementsByClassName("prev-btn")[0]
+    nextBtn = document.getElementsByClassName("next-btn")[0]
+    
+    prevBtn.addEventListener("click", () => { navigate(-AMOUNT_OF_DAYS) })
+    nextBtn.addEventListener("click", () => { navigate(+AMOUNT_OF_DAYS) })
 
-      const taskId = cellsWithTask[i].id
-      
-      for (let j = 0; j < assignedTasks.length; j++) {
-        if (taskId === assignedTasks[j].id) {
-          currentTask = assignedTasks[j]
-          tooltip.innerHTML = `
-                              ${currentTask.description}
-                              <br><br>
-                              срок ${currentTask.planEndDate}
-                              `
-        }
+    function shiftDates(amountOfDays) {
+      shiftedDates = [...dates]
+      for (i = 1; i < shiftedDates.length; i++) {
+        let timestamp = Date.parse(shiftedDates[i]) + 86400000 * amountOfDays
+        let newDate = new Date(timestamp)
+        date = newDate.toISOString().split('T')[0]
+        shiftedDates[i] = date
       }
-      cellsWithTask[i].appendChild(tooltip)
+      dates = [...shiftedDates]
+      return dates
     }
+
+    function navigate(days) {
+      updateHeaders(days)
+      createPrevButton()
+      createNextButton()
+      createCells()
+      assignTasks()
+      createTooltips()
+    }
+    
+    function updateHeaders(amountOfDays) {
+      shiftDates(amountOfDays)
+      
+      const headers = document.querySelectorAll('.table div .table__header')
+      headers.forEach((header, idx) => {
+        header.parentElement.classList = ''
+        if (dates[idx] === TODAY) {
+          header.parentElement.classList = 'today'
+        }
+        header.textContent = shiftedDates[idx]
+        header.classList = 'table__header'
+      })
+    }
+
   }
 
   createTable()
   createTableHeaders()
   createExecutors()
-  makePrevButton()
-  makeNextButton()
+  createPrevButton()
+  createNextButton()
   createCells()
   assignTasks()
-  createBacklog()
   createTooltips()
+  createBacklog()
+  createNavigation()
 }
 
 createApp()
